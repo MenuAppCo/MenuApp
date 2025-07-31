00ddb6262d5407c7af8380b6f37a53c9f4d6dde0
rojbar <55609284+rojbar@users.noreply.github.com>
2025-07-31 12:54:44 -0500

    feat implement iac admin frontend

---
+++ iac/admin-frontend/bucket.tf
@@ -0,0 +1,66 @@
+locals {
+  bucket_name = "${var.environment}-menupp-public-frontend"
+}
+
+resource "aws_s3_bucket" "frontend" {
+  bucket = local.bucket_name
+
+  tags = {
+    Environment = var.environment
+    App         = var.app
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  rule {
+    id     = "cleanup-old-versions"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "cloudfront_oac" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
---
+++ iac/admin-frontend/vars.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  description = "Environment name (e.g., dev, staging, prod)"
+  type        = string
+  default     = "production"
+}
+
+variable "app" {
+  description = "Application name"
+  type        = string
+  default     = "menupp"
+}
