27bbe646b96760bce355cf70e2b3d83f665ab73d
rojbar <55609284+rojbar@users.noreply.github.com>
2025-07-31 23:25:19 -0500

    feat implement public frontend s3, cloudfront distribution

--- iac/public-frontend/cloudfront.tf
+++ iac/public-frontend/cloudfront.tf
@@ -10,7 +10,7 @@ resource "aws_cloudfront_distribution" "frontend" {
   ]
 
   viewer_certificate {
-    acm_certificate_arn      = data.terraform_remote_state.route53.outputs.cert_validation.certificate_arn
+    acm_certificate_arn      = data.terraform_remote_state.route53.outputs.menapp_certificate_validation.certificate_arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
