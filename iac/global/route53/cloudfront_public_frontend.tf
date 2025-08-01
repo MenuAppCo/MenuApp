54a0a552fb5f75d51fee793b58e7e17e90e7b58e
rojbar <55609284+rojbar@users.noreply.github.com>
2025-07-31 22:42:06 -0500

    fix cd

--- iac/global/route53/cloudfront_public_frontend.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "cloudfront_lead_pages" {
-  backend = "s3"
-  config = {
-    bucket = "terraformstatetipiing"
-    key    = "leads/landing-pages-dashboard/env/project.tfstate"
-    region = "us-east-1"
-  }
-}
