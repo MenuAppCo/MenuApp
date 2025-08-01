3c2d87475155258aa98b613cd03a64cad8d886ac
rojbar <55609284+rojbar@users.noreply.github.com>
2025-08-01 12:26:39 -0500

    implement ci cd for route53Records

--- iac/global/route53Records/backend.tf
+++ iac/global/route53Records/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-menapp-production"
-    key            = "route53/project.tfstate"
+    key            = "route53Records/project.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
   }
