47a16c7b4984648fdef88d7575925da4334c266e
rojbar <55609284+rojbar@users.noreply.github.com>
2025-08-05 01:24:29 -0500

    remove global s3

--- iac/global/s3/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-menapp-production"
-    key            = "s3/project.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-  }
-}
