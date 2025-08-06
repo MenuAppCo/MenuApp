448c8bdfba68814d84b9e92949fb47455609c22b
rojbar <55609284+rojbar@users.noreply.github.com>
2025-08-06 14:39:44 -0500

    fix outputs apigw

--- iac/backend/apigw/outputs.tf
+++ iac/backend/apigw/outputs.tf
@@ -1,3 +1,7 @@
-output "admin_api_domain_cloudfront_domain_name" {
-  value = aws_api_gateway_domain_name.admin_api_domain.cloudfront_domain_name
+output "admin_api_domain_regional_domain_name" {
+  value = aws_api_gateway_domain_name.admin_api_domain.regional_domain_name
+}
+
+output "admin_api_domain_regional_zone_id" {
+  value = aws_api_gateway_domain_name.admin_api_domain.regional_zone_id
 }
--- iac/backend/outputs.tf
+++ iac/backend/outputs.tf
@@ -1,4 +1,7 @@
+output "admin_api_domain_regional_domain_name" {
+  value = module.apigw.admin_api_domain_regional_domain_name
+}
 
-output "admin_api_domain_cloudfront_domain_name" {
-  value = module.apigw.admin_api_domain_cloudfront_domain_name
+output "admin_api_domain_regional_zone_id" {
+  value = module.apigw.admin_api_domain_regional_zone_id
 }
