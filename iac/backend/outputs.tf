148be6ac4067eb9cabda7e079940ac8a6ca40855
rojbar <55609284+rojbar@users.noreply.github.com>
2025-08-06 14:34:51 -0500

    fix output

--- iac/backend/outputs.tf
+++ iac/backend/outputs.tf
@@ -1,4 +1,4 @@
 
 output "admin_api_domain_cloudfront_domain_name" {
-  value = module.api_gw.admin_api_domain_cloudfront_domain_name
+  value = module.apigw.admin_api_domain_cloudfront_domain_name
 }
