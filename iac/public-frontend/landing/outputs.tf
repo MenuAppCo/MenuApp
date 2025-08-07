dbfaafff999cc62600d3e543a5e372b604c00b8a
rojbar <55609284+rojbar@users.noreply.github.com>
2025-08-06 22:17:45 -0500

    fix output bucket regional domain name

--- iac/public-frontend/landing/outputs.tf
+++ iac/public-frontend/landing/outputs.tf
@@ -1,5 +1,5 @@
 output "frontend_bucket_regional_domain_name" {
-  value = aws_s3_bucket.frontend.regional_domain_name
+  value = aws_s3_bucket.frontend.bucket_regional_domain_name
 }
 
 output "frontend_bucket_id" {
