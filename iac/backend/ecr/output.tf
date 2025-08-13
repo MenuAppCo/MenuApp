20580df3e1ba823dc944d9c29086c9e8d66120e8
rojbar <55609284+rojbar@users.noreply.github.com>
2025-08-12 23:08:30 -0500

    implement ecr for public api lambda

--- iac/backend/ecr/output.tf
+++ iac/backend/ecr/output.tf
@@ -2,3 +2,8 @@ output "admin_api_lambda_ecr_url" {
   description = "admin api lambda repository"
   value       = aws_ecr_repository.admin_api_lambda.repository_url
 }
+
+output "public_api_lambda_ecr_url" {
+  description = "public api lambda repository"
+  value       = aws_ecr_repository.public_api_lambda.repository_url
+}
