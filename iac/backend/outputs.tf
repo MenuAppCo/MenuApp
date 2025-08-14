c072c5a3c898f8d34eba0f3e16cfe5edad9c6d37
rojbar <55609284+rojbar@users.noreply.github.com>
2025-08-13 20:50:07 -0500

    remove temp outputs

--- iac/backend/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "admin_api_domain_regional_domain_name" {
-  value = module.apigw.admin_api_domain_regional_domain_name
-}
-
-output "admin_api_domain_regional_zone_id" {
-  value = module.apigw.admin_api_domain_regional_zone_id
-}
