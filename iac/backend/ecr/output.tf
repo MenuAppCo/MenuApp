f0b79757b8fdda64ad40838469c01ee1419f028e
rojbar <55609284+rojbar@users.noreply.github.com>
2025-08-24 12:55:10 -0500

    correct uri

--- iac/backend/ecr/output.tf
+++ iac/backend/ecr/output.tf
@@ -1,9 +1,9 @@
 output "admin_api_lambda_ecr_url" {
   description = "admin api lambda repository"
-  value       = aws_ecr_repository.admin_api_lambda.repository_url
+  value       = aws_ecr_repository.admin_api_lambda.repository_uri
 }
 
 output "public_api_lambda_ecr_url" {
   description = "public api lambda repository"
-  value       = aws_ecr_repository.public_api_lambda.repository_url
+  value       = aws_ecr_repository.public_api_lambda.repository_uri
 }
